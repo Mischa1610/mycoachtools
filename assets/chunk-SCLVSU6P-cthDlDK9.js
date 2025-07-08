081654f1565c7f5081c114c47742e913d686b952
Michael Alekseew <alekseew.m@googlemail.com>
2025-07-08 18:21:10 +0200

    Updates

---
+++ assets/chunk-SCLVSU6P-cthDlDK9.js
@@ -0,0 +1,15 @@
+import{_ as e}from"./md-Dkj8x4Rj.js";var l=e(()=>`
+  /* Font Awesome icon styling - consolidated */
+  .label-icon {
+    display: inline-block;
+    height: 1em;
+    overflow: visible;
+    vertical-align: -0.125em;
+  }
+  
+  .node .label-icon path {
+    fill: currentColor;
+    stroke: revert;
+    stroke-width: revert;
+  }
+`,"getIconStyles");export{l as g};
---
+++ assets/chunk-YJGJQOYZ-D1D54-qm.js
@@ -0,0 +1,1 @@
+import{_ as s}from"./md-Dkj8x4Rj.js";var t,e=(t=class{constructor(i){this.init=i,this.records=this.init()}reset(){this.records=this.init()}},s(t,"ImperativeState"),t);export{e as I};
